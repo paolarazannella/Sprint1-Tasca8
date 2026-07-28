0084c13f715b9f33b79b268b7cc6ac307efc59b0
Paola <plara222@gmail.com>
2026-07-28 10:19:44 +0200

    exercici1

---
+++ src/exercici1.php
@@ -0,0 +1,14 @@
+<?php
+class NumberChecker  {
+
+
+	public function __construct(private int $number){}
+
+	public function isEven(): bool {
+		return $this->number%2 == 0;
+	}
+	public function isPositive(): bool {
+		return $this->number > 0;
+	}
+
+}
---
+++ tests/Test.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../src/exercici1.php';
+
+class NumberCheckerTest extends TestCase
+{
+    public function testNumberIsEven(): void
+    {
+        $checker = new NumberChecker(4);
+        $result = $checker->isEven();
+
+        $this->assertTrue($result);
+    }
+
+    public function testNumberIsNotEven(): void
+    {
+        $checker = new NumberChecker(9);
+        $result = $checker->isEven();
+        $this->assertFalse($result);
+    }
+
+    public function testNumberIsPositive(): void
+    {
+        $checker = new NumberChecker(2);
+        $result = $checker->isPositive();
+        $this->assertTrue($result);
+    }
+
+    public function testNumberIsNotPositive(): void
+    {
+        $checker = new NumberChecker(-9);
+        $result = $checker->isPositive();
+        $this->assertFalse($result);
+    }
+
+}
